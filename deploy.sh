65b3008da9329b9f7e1715b6d2c8aa600920244a
lxwcodemonkey <jolunoluo@tencent.com>
2024-06-16 21:51:36 +0800

    fix deploy.sh failed

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+rm -rf categories
+rm -rf docs
+rm -rf fonts
+rm -rf img
+rm -rf katex
+rm -rf posts
+rm -rf svg
+rm -rf tags
 cd supersonic_doc
 hugo --theme=hugo-book --baseURL="https://supersonicbi.github.io/" --buildDrafts
 mv public/* ../
